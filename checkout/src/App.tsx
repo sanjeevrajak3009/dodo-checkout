9057f0a868a1de493255d12a81a6f6c4a2d69733
Sanjeev Rajak <142082504+sanjeevrajak3009@users.noreply.github.com>
2026-09-11 14:36:26 +0530

    Remove height class from main element

--- checkout/src/App.tsx
+++ checkout/src/App.tsx
@@ -262,7 +262,7 @@ useEffect(() => {
    * Checkout
    */
   return (
-    <main className="h-screen bg-transparent  p-2">
+    <main className=" bg-transparent  p-2">
       <section className="mx-auto flex h-full w-full max-w-md flex-col overflow-hidden rounded-2xl bg-white shadow-2xl">
 
         {/* Dodo Header */}
